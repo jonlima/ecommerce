dbd5e37895d3ff64820dc40159ffb40f987696d4
Jonatha Lima <jonatha.lima14@gmail.com>
2024-09-09 15:51:44 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // adicionar bearer auth
+  const configSwagger = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('Loomi Challenge')
+    .setVersion('0.1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('doc', app, swaggerDocument, {
+    jsonDocumentUrl: 'doc/json',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { GetHealthStatusUseCase } from './use-cases/get-health-status.use-case';
 import { HealthStatusDto } from './dto/health-status.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
   constructor(
@@ -9,6 +11,8 @@ export class HealthController {
   ) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get the current health status of the application' })
+  @ApiResponse({ type: HealthStatusDto })
   getHealthStatus(): HealthStatusDto {
     return this.getHealthStatusUseCase.execute();
   }
