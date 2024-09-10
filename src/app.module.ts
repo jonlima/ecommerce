ee6e0dc68a44079a97539c6147d517c8da92e996
Jonatha Lima <jonatha.lima14@gmail.com>
2024-09-10 08:16:17 -0300

    feat: add typeorm configuration and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
+import { TypeormPersistenceModule } from './shared/infra/persistence/typeorm/typeorm-persistence.module';
 
 @Module({
-  imports: [HealthModule, EnvConfigModule.forRoot()],
+  imports: [HealthModule, EnvConfigModule.forRoot(), TypeormPersistenceModule],
   controllers: [],
   providers: [],
 })
